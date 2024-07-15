a68b05f8b3515b48a7eea1bd4b3b2d5bcb6ece17
yuvaraj-p <it-software14@apachefootwear.com>
2024-07-15 17:00:59 +0530

    Send Images Success in Chat Screen

--- lib/screens/home_screen/chat_screen.dart
+++ lib/screens/home_screen/chat_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:animated_movies_app/constants/ui_constant.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
@@ -65,18 +69,27 @@ class _ChatScreenState extends State<ChatScreen> {
       final response = await http.get(Uri.parse(url));
 
       print('Request URL: $url');
-      print('Response: ${response.body}');
+      // print('Response: ${response.body}');
+      print('Response Body: ${response.body}');
 
       if (response.statusCode == 200) {
         // Check if response body is not null
         if (response.body != null) {
           List<dynamic> messagesJson = jsonDecode(response.body);
+
           setState(() {
             _messages.clear();
             _messages.addAll(messagesJson
                 .map((message) => ChatMessage.fromJson(message))
                 .toList());
+            // print(_messages);
+            // _messages.forEach((element) {
+            //   Uint8List? _bytesImages =
+            //       Base64Decoder().convert(element.fileBytesBase64);
+            //   element.fileBytesBase64 = _bytesImages;
+            // });
             // Update button visibility based on messages list
+
             if (_messages.isNotEmpty) {
               buttonVisibility = [false, false, false]; // Hide buttons
             } else {
@@ -95,62 +108,106 @@ class _ChatScreenState extends State<ChatScreen> {
     }
   }
 
-  Future<void> sendMessage(String text, int index) async {
+  Future<void> _sendMessageOrFile({String? text, File? file}) async {
     try {
-      if (text.isNotEmpty) {
-        final String url = 'http://10.3.0.70:9040/api/Flutter/AddMessageAsync';
-        final Map<String, dynamic> requestBody = {
-          'SenderId': widget.senderId,
-          'ReceiverId': widget.receiverId,
-          'MessageText': text,
-          'IsUserMessage': true,
-          'MessageTimestamp': DateTime.now().toIso8601String(),
-        };
+      final String url = 'http://10.3.0.70:9040/api/Flutter/AddMessageAsync';
+      final request = http.MultipartRequest('POST', Uri.parse(url))
+        ..fields['SenderId'] = widget.senderId.toString()
+        ..fields['ReceiverId'] = widget.receiverId.toString()
+        ..fields['MessageText'] =
+            text ?? '' // Use provided text or empty string
+        ..fields['IsUserMessage'] = 'true'
+        ..fields['MessageTimestamp'] = DateTime.now().toIso8601String();
 
-        print('Request URL: $url');
-        print('Request Body: ${jsonEncode(requestBody)}');
+      String? fileType;
+      String? fileBytesBase64;
+      if (file != null) {
+        final fileName = file.path.split('/').last;
+        fileType = fileName.split('.').last.toLowerCase();
 
-        final response = await http.post(
-          Uri.parse(url),
-          headers: <String, String>{
-            'Content-Type': 'application/json; charset=UTF-8',
-          },
-          body: jsonEncode(requestBody),
-        );
+        // Read the file bytes and convert to Base64
+        final Uint8List bytes = await file.readAsBytes();
+        fileBytesBase64 = base64.encode(bytes);
+        print('base 64 : ${fileBytesBase64}');
 
-        print('Response Status: ${response.statusCode}');
-        print('Response Body: ${response.body}');
+        request.fields['FileBytesBase64'] =
+            fileBytesBase64; // Include Base64 in the request
 
-        if (response.statusCode == 200) {
-          // Message sent successfully
+        request.files.add(await http.MultipartFile.fromPath('file', file.path,
+            filename: fileName));
+      }
 
-          _controller.clear();
-          setState(() {
-            _messages.insert(
-              0,
-              ChatMessage(
-                messageId: _messages.length + 1,
-                senderId: widget.senderId,
-                receiverId: widget.receiverId,
-                messageText: text,
-                messageTimestamp: DateTime.now(),
-                isUserMessage: true,
-              ),
-            );
-            buttonVisibility[index] = false; // Hide button after sending
-          });
-        } else {
-          // Handle HTTP error
-          final responseBody = jsonDecode(response.body);
-          print('Failed to send message: ${response.statusCode}');
-          print('Error: ${responseBody['Message']}');
-          if (responseBody.containsKey('Error')) {
-            print('Detailed Error: ${responseBody['Error']}');
+      final response = await request.send();
+      final responseBody = await response.stream.bytesToString();
+
+      if (response.statusCode == 200) {
+        print(file != null
+            ? 'File sent successfully'
+            : 'Message sent successfully');
+        _controller.clear();
+        // setState(() {
+        //   _messages.insert(
+        //     0,
+        //     ChatMessage(
+        //       messageId: _messages.length + 1,
+        //       senderId: widget.senderId,
+        //       receiverId: widget.receiverId,
+        //       messageText: file != null
+        //           ? 'Sent a file: ${file.path.split('/').last}'
+        //           : text!,
+        //       messageTimestamp: DateTime.now(),
+        //       isUserMessage: true,
+        //       isFile: file != null,
+        //       fileType: fileType,
+        //       fileUrl: file?.path,
+        //       fileBytesBase64: fileBytesBase64 ?? '', // Store Base64 string
+        //     ),
+        //   );
+        //   if (file != null) {
+        //     buttonVisibility[0] = false; // Hide button after sending file
+        //   }
+        // });
+
+        setState(() {
+          _messages.insert(
+            0,
+            ChatMessage(
+              messageId: _messages.length + 1,
+              senderId: widget.senderId,
+              receiverId: widget.receiverId,
+              messageText: file != null
+                  ? 'Sent a file: ${file.path.split('/').last}'
+                  : text!,
+              messageTimestamp: DateTime.now(),
+              isUserMessage: true,
+              isFile: file != null, // Set isFile
+              fileType: fileType, // Set fileType
+              fileUrl: file?.path,
+              fileBytesBase64: fileBytesBase64 ?? '', // Store Base64 string
+            ),
+          );
+          if (file != null) {
+            buttonVisibility[0] = false; // Hide button after sending file
           }
-        }
+        });
+      } else {
+        print(
+            'Failed to send ${file != null ? 'file' : 'message'}: ${response.statusCode}');
+        print('Response Body: $responseBody');
       }
     } catch (e) {
-      print('Error sending message: $e');
+      print('Error sending ${file != null ? 'file' : 'message'}: $e');
+    }
+  }
+
+  Future<void> _pickAndSendFile() async {
+    final result = await FilePicker.platform.pickFiles();
+    if (result != null) {
+      final PlatformFile file = result.files.first;
+      final selectedFile = File(file.path!);
+
+      // Call the unified method to handle file sending
+      await _sendMessageOrFile(file: selectedFile);
     }
   }
 
@@ -204,7 +261,10 @@ class _ChatScreenState extends State<ChatScreen> {
       receiverId: widget.senderId, // Receiver is the sender in this context
       messageText: text,
       messageTimestamp: DateTime.now(), // Use appropriate timestamp
-      isUserMessage: false, // Received messages are not user messages
+      isUserMessage: false,
+      // fileUrl: null,
+      fileBytesBase64: '',
+      fileUrl: null, // Received messages are not user messages
     );
 
     setState(() {
@@ -245,7 +305,10 @@ class _ChatScreenState extends State<ChatScreen> {
                 if (buttonVisibility[0])
                   GestureDetector(
                     onTap: () {
-                      sendMessage('How are you?', 0);
+                      // sendMessage('How are you?', 0);
+                      _sendMessageOrFile(
+                          text:
+                              'How are you?'); // Send predefined message 'Hi!'
                     },
                     child: Container(
                       margin: const EdgeInsets.only(bottom: 8.0, right: 8.0),
@@ -263,7 +326,8 @@ class _ChatScreenState extends State<ChatScreen> {
                 if (buttonVisibility[1])
                   GestureDetector(
                     onTap: () {
-                      sendMessage('Hi!', 1);
+                      // sendMessage('Hi!', 1);
+                      _sendMessageOrFile(text: 'Hi!');
                     },
                     child: Container(
                       margin: const EdgeInsets.only(bottom: 8.0, right: 8.0),
@@ -281,7 +345,8 @@ class _ChatScreenState extends State<ChatScreen> {
                 if (buttonVisibility[2])
                   GestureDetector(
                     onTap: () {
-                      sendMessage('What\'s up?', 2);
+                      // sendMessage('What\'s up?', 2);
+                      _sendMessageOrFile(text: 'What\'s up?');
                     },
                     child: Container(
                       margin: const EdgeInsets.only(bottom: 8.0, right: 8.0),
@@ -311,6 +376,61 @@ class _ChatScreenState extends State<ChatScreen> {
     );
   }
 
+  // Widget _buildMessage(ChatMessage message) {
+  //   return Align(
+  //     alignment: message.senderId == widget.senderId
+  //         ? Alignment.centerRight
+  //         : Alignment.centerLeft,
+  //     child: Container(
+  //       margin: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+  //       padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 15.0),
+  //       decoration: BoxDecoration(
+  //         color: message.senderId == widget.senderId
+  //             ? Colors.lightGreen
+  //             : Colors.grey,
+  //         borderRadius: BorderRadius.circular(10.0),
+  //       ),
+  //       child: IntrinsicWidth(
+  //         child: Column(
+  //           crossAxisAlignment: CrossAxisAlignment.start,
+  //           children: [
+  //             if (message.isFile && message.fileType != null) ...[
+  //               if (['jpg', 'jpeg', 'png'].contains(message.fileType) &&
+  //                   message.fileBytesBase64 != null &&
+  //                   message.fileBytesBase64!.isNotEmpty)
+  //                 Image.memory(
+  //                   base64Decode(message.fileBytesBase64!),
+  //                   height: 100,
+  //                   width: 100,
+  //                   fit: BoxFit.cover,
+  //                 )
+  //               else
+  //                 const SizedBox
+  //                     .shrink(), // Hide if not an image or base64 is null
+  //             ],
+  //             if (!message.isFile)
+  //               Text(
+  //                 message.messageText ?? '', // Default to empty string if null
+  //                 style: const TextStyle(
+  //                   color: Colors.white,
+  //                 ),
+  //               ),
+  //             Text(
+  //               DateFormat('hh:mm a').format(message.messageTimestamp),
+  //               style: const TextStyle(
+  //                 color: Colors.white60,
+  //                 fontSize: 12.0,
+  //               ),
+  //             ),
+  //           ],
+  //         ),
+  //       ),
+  //     ),
+  //   );
+  // }
+
+//here we show only image and text inside chat screen
+
   Widget _buildMessage(ChatMessage message) {
     return Align(
       alignment: message.senderId == widget.senderId
@@ -329,20 +449,30 @@ class _ChatScreenState extends State<ChatScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(
-                message.messageText,
-                style: const TextStyle(
+              // Display image if it exists
+              if (message.fileBytesBase64 != null &&
+                  message.fileBytesBase64!.isNotEmpty)
+                Image.memory(
+                  base64Decode(message.fileBytesBase64!),
+                  height: 100,
+                  width: 100,
+                  fit: BoxFit.cover,
+                ),
+              // Display text message if it exists
+              if (message.messageText != null &&
+                  message.messageText!.isNotEmpty)
+                Text(
+                  message.messageText!,
+                  style: const TextStyle(
                     color: Colors.white,
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
-                    fontStyle: FontStyle.italic),
-              ),
-              // const SizedBox(height: 12), // Adjust spacing as needed
-              Align(
-                alignment: Alignment.bottomRight,
-                child: Text(
-                  '${DateFormat('hh:mm a').format(message.messageTimestamp)}',
-                  style: const TextStyle(color: Colors.white, fontSize: 12),
+                  ),
+                ),
+              // Display timestamp
+              Text(
+                DateFormat('hh:mm a').format(message.messageTimestamp),
+                style: const TextStyle(
+                  color: Colors.white60,
+                  fontSize: 12.0,
                 ),
               ),
             ],
@@ -359,13 +489,18 @@ class _ChatScreenState extends State<ChatScreen> {
         margin: const EdgeInsets.symmetric(horizontal: 8.0),
         child: Row(
           children: <Widget>[
+            IconButton(
+              icon: const Icon(Icons.attach_file), // Add a file icon
+              onPressed: _pickAndSendFile, // Call file picker method
+            ),
             Expanded(
               child: TextField(
                 controller: _controller,
                 // onSubmitted: sendMessage,
 
                 onSubmitted: (text) {
-                  sendMessage(text, -1); // Use -1 for custom message
+                  // sendMessage(text, -1); // Use -1 for custom message
+                  _sendMessageOrFile(text: text); // Use unified method for text
                 },
                 decoration: const InputDecoration.collapsed(
                   hintText: 'Send a message',
@@ -375,7 +510,11 @@ class _ChatScreenState extends State<ChatScreen> {
             IconButton(
               icon: const Icon(Icons.send),
               // onPressed: () => sendMessage(_controller.text),
-              onPressed: () => sendMessage(_controller.text, -1),
+              // onPressed: () => sendMessage(_controller.text, -1),
+              onPressed: () {
+                _sendMessageOrFile(
+                    text: _controller.text); // Use unified method for text
+              },
             ),
           ],
         ),
@@ -384,13 +523,78 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 }
 
+// class ChatMessage {
+//   final int messageId;
+//   final int senderId;
+//   final int receiverId;
+//   final String messageText;
+//   final DateTime messageTimestamp;
+//   final bool isUserMessage;
+//   final bool isFile;
+//   final String? fileType;
+//   final String? fileUrl;
+//   final String? fileBytesBase64; // Expect Base64 string
+
+//   ChatMessage({
+//     required this.messageId,
+//     required this.senderId,
+//     required this.receiverId,
+//     required this.messageText,
+//     required this.messageTimestamp,
+//     required this.isUserMessage,
+//     this.isFile = false,
+//     this.fileType,
+//     this.fileUrl,
+//     this.fileBytesBase64,
+//   });
+
+//   factory ChatMessage.fromJson(Map<String, dynamic> json) => ChatMessage(
+//         messageId: json['MessageId'],
+//         senderId: json['SenderId'],
+//         receiverId: json['ReceiverId'],
+//         messageText: json['MessageText'],
+//         messageTimestamp: DateTime.parse(json['MessageTimestamp']),
+//         isUserMessage: json['IsUserMessage'],
+//         fileUrl: json['FileUrl'],
+//         fileBytesBase64: json['FileBytesBase64'],
+//       );
+
+//   // factory ChatMessage.fromJson(Map<String, dynamic> json) => ChatMessage(
+//   //       messageId: json['MessageId'],
+//   //       senderId: json['SenderId'],
+//   //       receiverId: json['ReceiverId'],
+//   //       messageText:
+//   //           json['MessageText'] ?? '', // Default to empty string if null
+//   //       messageTimestamp: DateTime.parse(json['MessageTimestamp']),
+//   //       isUserMessage: json['IsUserMessage'],
+//   //       fileUrl: json['FileUrl'],
+//   //       fileBytesBase64: json['FileBytesBase64'],
+//   //     );
+// }
+
+// To parse this JSON data, do
+//
+//     final chatMessage = chatMessageFromJson(jsonString);
+
+List<ChatMessage> chatMessageFromJson(String str) => List<ChatMessage>.from(
+    json.decode(str).map((x) => ChatMessage.fromJson(x)));
+
+String chatMessageToJson(List<ChatMessage> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
 class ChatMessage {
-  final int messageId;
-  final int senderId;
-  final int receiverId;
-  final String messageText;
-  final DateTime messageTimestamp;
-  final bool isUserMessage;
+  int messageId;
+  int senderId;
+  int receiverId;
+  dynamic messageText;
+  DateTime messageTimestamp;
+  bool isUserMessage;
+  dynamic fileUrl;
+  String? fileBytesBase64;
+  // dynamic fileBytesBase64;
+
+  bool isFile; // Add this property
+  String? fileType; // Add this property
 
   ChatMessage({
     required this.messageId,
@@ -399,7 +603,24 @@ class ChatMessage {
     required this.messageText,
     required this.messageTimestamp,
     required this.isUserMessage,
+    this.fileUrl,
+    this.fileBytesBase64,
+    this.isFile = false, // Default to false
+    this.fileType,
   });
+
+  // factory ChatMessage.fromJson(Map<String, dynamic> json) => ChatMessage(
+  //       messageId: json["MessageId"],
+  //       senderId: json["SenderId"],
+  //       receiverId: json["ReceiverId"],
+  //       messageText: json["MessageText"],
+  //       messageTimestamp: DateTime.parse(json["MessageTimestamp"]),
+  //       isUserMessage: json["IsUserMessage"],
+  //       fileUrl: json["FileUrl"],
+  //       fileBytesBase64: json["FileBytesBase64"],
+  //       isFile: json["IsFile"] ?? false, // Deserialize the isFile property
+  //       fileType: json["FileType"], // Deserialize the fileType property
+  //     );
 
   factory ChatMessage.fromJson(Map<String, dynamic> json) => ChatMessage(
         messageId: json['MessageId'],
@@ -408,5 +629,22 @@ class ChatMessage {
         messageText: json['MessageText'],
         messageTimestamp: DateTime.parse(json['MessageTimestamp']),
         isUserMessage: json['IsUserMessage'],
+        isFile: json['IsFile'] ?? false,
+        fileType: json['FileType'],
+        fileBytesBase64:
+            json['FileBytesBase64'], // Ensure this is correctly mapped
       );
+
+  Map<String, dynamic> toJson() => {
+        "MessageId": messageId,
+        "SenderId": senderId,
+        "ReceiverId": receiverId,
+        "MessageText": messageText,
+        "MessageTimestamp": messageTimestamp.toIso8601String(),
+        "IsUserMessage": isUserMessage,
+        "FileUrl": fileUrl,
+        "FileBytesBase64": fileBytesBase64,
+        "IsFile": isFile, // Serialize the isFile property
+        "FileType": fileType, // Serialize the fileType property
+      };
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import emoji_picker_flutter
+import file_selector_macos
 import flutter_inappwebview_macos
 import flutter_local_notifications
 import path_provider_foundation
@@ -13,6 +15,8 @@ import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   InAppWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "InAppWebViewFlutterPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
